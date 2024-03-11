6cd687fd599bed9fb7e9b1071705e0516b9a7881
ZJY25 <2297666392@qq.com>
2024-03-11 20:34:24 +0800

    business separate class

--- business.py
+++ business.py
@@ -1,23 +1,43 @@
-business_type = ["回网", "预离网回网",
-                 "全能卡"
+import openpyxl
+business_type = ["回网",
+                 "预离",
+                 "全能卡",
                  "88", "118", "158", "188", "228", "288", "388", "39", "79", "119", "229",
                  "单移", "双百", "副卡"]
+net_back_key_words = ["互", "宽", "高", "有效", "标清"]
 
 
 def judge_business_type(business_str):
     for key, value in enumerate(business_type):
-        if business_str.find(value):
-            print(business_str.find(value))
+        flag = business_str.find(value)
+        if flag >= 0:
             print(business_str + " " + value + " " + str(key))
             return key
 
+def judge_net_back_type(net_back_str):
+    for key, value in enumerate(net_back_key_words):
+        flag = net_back_str.find(value)
+        if flag >= 0:
+            print(business_str + " " + value + " " + str(key))
+            return key
 
-def business_process(business_str):
+def business_process(business_str, excel, i, j):
     b_type = judge_business_type(business_str)
-    print(b_type)
-    if b_type in (0, 1):
+    print("b_type: " + str(b_type))
+    if b_type == 0:
         print("回网")
+        if [0] in business_str:
+            excel.write_cell(i, j, business_str)
+            print("写入excel")
+    if b_type == 1:
+        print("预离")
+    if b_type == 2:
+        print("全能卡")
+    if b_type in range(3, 14):
+        print("数字新增")
+    if b_type in range(14, 17):
+        print("文字新增")
 
 
 if __name__ == '__main__':
-    business_process("回网高互宽")
+    business_process("副卡")
--- main.py
+++ main.py
@@ -2,61 +2,41 @@ import os
 import openpyxl
 from openpyxl.utils import get_column_letter
 import re
+import business as bp
+from excel_process import ExcelProcess
 
 path = r"D:\projects\split_data\split_data"
-os.chdir(path)  # 修改工作路径
+filename = "test.xlsx"
+table_name = "test"
+name_pattern = "发展人[:：,，][（）\d\u4e00-\u9fa5]+"
+date_pattern = "\d+月\d+日"
+business_pattern = "(?<=业务[:：])[\d\u4e00-\u9fa5]+(?=[,，])"
 
-workbook = openpyxl.load_workbook('test.xlsx')
-# print(workbook.sheetnames)  # 查看work book的表格
+excel = ExcelProcess(path, filename)
+excel.load_excel_sheet(table_name)
+for row in range(5, 10):
 
-sheet = workbook['test']
-# print(sheet)
-
-max_row = sheet.max_row
-print("max_row", max_row)  # 获取最大行数
-
-column_item = sheet['A1':'A%d' % max_row]
-print("column_item", column_item)
-
-business_type = ["88", "118", "158", "188", "228", "288", "388", "39", "79", "119", "229",
-                 "单移", "双百", "副卡", "全能卡99", "全能卡129", "全能卡169", "全能卡229",
-                 "高互宽回网", "有效回网", "高清回网", "标清回网", "互动回网", "宽带回网",
-                 "有效预离网回网", "宽带预离网回网", "高互宽预离网回网"]
-
-
-for key, value in enumerate(business_type):
-    print(str(key) + ":" + value)
-
-for i in range(5, 10):
     column = 2
+    all_string = excel.get_cell(row, 1)
 
-    all_string = sheet.cell(row=i, column=1).value
-    print(str(i), ":", all_string)
-
-    # ((\d*) | ([(（][\u4e00-\u9fa5]+[)）])*)
-    names = re.findall('发展人[:：,，][（）\d\u4e00-\u9fa5]+', all_string)
+    names = re.findall(name_pattern, all_string)
     for name in names:
         name = re.findall("(?<=[:：,，]).*$", name)
         name_str = name[0]
-        column_letter = get_column_letter(column)
-        print(column_letter + str(i) + ":" + name_str)
-        sheet[column_letter + str(i)] = name_str
+        excel.write_cell(row, column, name_str)
         column += 1
 
     column = 4
 
-    date = re.findall('\d+月\d+日', all_string)
+    date = re.findall(date_pattern, all_string)
     date_str = date[0]
-    column_letter = get_column_letter(column)
-    print(column_letter + str(i) + ":" + date_str)
-    sheet[column_letter + str(i)] = date_str
+    excel.write_cell(row, column,date_str)
     column += 1
 
-    business = re.findall('(?<=业务[:：])[\d\u4e00-\u9fa5]+(?=[,，])', all_string)
+    business = re.findall(business_pattern, all_string)
     business_str = business[0]
-    column_letter = get_column_letter(column)
-    print(column_letter + str(i) + ":" + business_str)
-    sheet[column_letter + str(i)] = business_str
+    excel.write_cell(row, column, business_str)
     column += 1
+    bp.business_process(business_str, excel, row, column)
 
-# workbook.save('test.xlsx')
+excel.save_excel(filename)
